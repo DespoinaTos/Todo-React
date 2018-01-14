b40254d3813abcf862d17a94fae6d8f89e90644f
Despoina Tosounidou <despoina.tosounidou@gmail.com>
2018-01-14 20:10:10 +0200

    build the addtodo component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import Header from './header';
+import AddTodo from './add_todo';
 
 class App extends Component {
+constructor(props) {
+  super(props);
+
+  this.state = { 
+    todos: [], 
+  };
+}
+
   render() {
     return (
       <div className="App">
        <Header />
+       <AddTodo />
       </div>
     );
   }
---
+++ src/components/add_todo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddTodo extends Component {
+
+    render() {
+        return(
+           <input type="text"/>
+        );
+    }
+
+}
+
+export default AddTodo;
