e63f343fa4a4e845f024d44d99c1f5a97c8b400a
Despoina Tosounidou <despoina.tosounidou@gmail.com>
2018-01-16 00:16:19 +0200

    added the delete todo funcionallity

--- src/components/add_todo.js
+++ src/components/add_todo.js
@@ -18,18 +18,30 @@ class AddTodo extends Component {
 	onClickAdd = () => {
 		const newtodos = this.state.todos.slice();
 		newtodos.push(this.state.currentTodo);
-		this.setState({todos : newtodos})
+		this.setState({
+			todos : newtodos,
+			currentTodo: '',
+		});
 	};
+
+	onClickDelete = (i) => {
+		const newtodosD = this.state.todos.slice();
+		newtodosD.splice(i ,1);
+		this.setState({
+			todos: newtodosD,
+		})
+	}
 
 	render() {
 		let listTodosItems = this.state.todos.map((e, i) => {
 			return (
-				<li>{e}</li>
+				<li key={i}> {e}<button onClick={this.onClickDelete}>delete</button> </li>
 			);
 		});
+
 		return(
 			<div>
-				<input onChange={this.onInputAdd} type="text" placeholder="add new todo..."/>
+				<input onChange={this.onInputAdd} value={this.state.currentTodo} type="text" placeholder="add new todo..."/>
 				<button onClick={this.onClickAdd}>Add</button>
 				<br/>
 				{this.state.todos.length === 0 ? 'Add a new todo' : 'You have some todos to do'}
